b180e6fffd4d59b7a0f4873c746981cf8947c4f5
juneyoshii <juney37@gmail.com>
2018-05-13 23:47:10 -0700

    experimenting with JS and objects

--- store/js/script.js
+++ store/js/script.js
@@ -1,9 +1,21 @@
-//Define buttons variable and category number variable
+//Define global variables and objects
 var buttons = document.getElementsByTagName('button');
 
 var items = document.getElementsByClassName('item');
 
 var categories = document.getElementsByClassName('itemList');
+
+var shoppingCart = {};
+
+var itemName, category, price;
+
+var ShoppingCartItem = function(category, price) {
+  // ShoppingCartItem.numInstances = (ShoppingCartItem.numInstances || 0 ) + 1;
+  // var quantity = ShoppingCartItem.numInstances;
+  this.category = category;
+  this.price = price;
+  this.quantity = 1;
+};
 
 // Add event listeners to buttons
 for (var i = 0; i < buttons.length; i++) {
@@ -44,17 +56,36 @@ function defaultView() {
     items[i].addEventListener('click', addItem);
   }
 
+  //function addToShoppingCart to keep track of quantities - an experiemnt
+  function addToShoppingCart(itemName, category, price) {
+    if (!shoppingCart[itemName]) {
+      shoppingCart[itemName] = new ShoppingCartItem(category, price);
+    }
+    else if (shoppingCart[itemName]) {
+      shoppingCart[itemName].quantity++;
+    }
+  };
+
 // define function to add item to shopping list
 function addItem() {
   var shoppingList = document.getElementById('shoppingList');
 
-  var itemName = this.getElementsByTagName('p')[0].innerText;
+  itemName = this.getElementsByTagName('p')[0].innerText;
+
+  category = this.parentNode.parentNode.id;
+
+  price =
+  this.getElementsByTagName('p')[1].innerText;
 
   var itemToAdd = document.createElement('li');
 
-  itemToAdd.textContent = itemName;
+  itemToAdd.textContent = itemName + ' - ' + price;
 
   shoppingList.appendChild(itemToAdd);
-}
+
+//experiment to see if I can create a shopping cart object
+  addToShoppingCart(itemName, category, price);
+};
+
 
 defaultView();
